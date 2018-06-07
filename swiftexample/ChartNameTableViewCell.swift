676e8b0c2eb15e2e7bbd250c50dbf753e6281370
Kyle Everson <kyle.everson@gmail.com>
2018-06-06 23:12:31 -0700

    Create table view to eventually show the chart list.

---
+++ swiftexample/ChartNameTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChartNameTableViewCell.swift
+//  swiftexample
+//
+//  Created by Kyle Everson on 6/6/18.
+//  Copyright © 2018 Kyle Everson. All rights reserved.
+//
+
+import UIKit
+
+class ChartNameTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
